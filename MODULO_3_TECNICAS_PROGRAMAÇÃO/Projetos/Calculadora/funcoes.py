1f033343e271f0a550ffec0a0c610e0e7dc7ed94
Kelvin Pichinini <kelvinpichinini@gmail.com>
2023-03-29 10:41:05 -0300

    Type hints and docstrings adicionados

--- MODULO_3_TECNICAS_PROGRAMAÇÃO/Projetos/Calculadora/funcoes.py
+++ MODULO_3_TECNICAS_PROGRAMAÇÃO/Projetos/Calculadora/funcoes.py
@@ -1,18 +1,21 @@
-def soma(a,b):
+def soma(a:float,b:float) -> float:
+    '''Takes the numbers a and b and returns the sum of them'''
     try:
         return a + b
     except TypeError:
         print(f"O input 'a' e 'b' devem ser números, recebido {a}: {type(a)}, {b}: {type(b)} ")
         return 'ERRO'
 
-def subtracao(a,b):
+def subtracao(a:float,b:float) -> float:
+    '''Takes the numbers a and b and returns the subtraction of them'''
     try:
         return a - b
     except TypeError:
         print(f"O input 'a' e 'b' devem ser números, recebido {a}: {type(a)}, {b}: {type(b)} ")
         return 'ERRO'
     
-def divisao(a,b):
+def divisao(a:float,b:float) -> float:
+    '''Takes the numbers a and b and returns the division of them'''
     if b == 0:
         return 0
     else:
@@ -22,7 +25,8 @@ def divisao(a,b):
             print(f"O input 'a' e 'b' devem ser números, recebido {a}: {type(a)}, {b}: {type(b)} ")
             return 'ERRO' 
         
-def multiplicacao(a,b):
+def multiplicacao(a:float,b:float) -> float:
+    '''Takes the numbers a and b and returns the multiplication of them'''
     try:
         return a*b
     except TypeError:
