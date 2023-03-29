fd5c63f40a9386a9d5e3e9e1872a718afc50e6eb
Kelvin Pichinini <kelvinpichinini@gmail.com>
2023-03-29 09:58:57 -0300

    Função subtração criada

--- MODULO_3_TECNICAS_PROGRAMAÇÃO/Projetos/Calculadora/funcoes.py
+++ MODULO_3_TECNICAS_PROGRAMAÇÃO/Projetos/Calculadora/funcoes.py
@@ -4,3 +4,10 @@ def soma(a,b):
     except TypeError:
         print(f"O input 'a' e 'b' devem ser números, recebido {a}: {type(a)}, {b}: {type(b)} ")
         return 'ERRO'
+
+def subtracao(a,b):
+    try:
+        return a - b
+    except TypeError:
+        print(f"O input 'a' e 'b' devem ser números, recebido {a}: {type(a)}, {b}: {type(b)} ")
+        return 'ERRO'
