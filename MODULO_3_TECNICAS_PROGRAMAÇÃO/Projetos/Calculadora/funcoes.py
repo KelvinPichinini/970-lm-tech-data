7dc7a8d00aeb23c25f4182ab5b7e5d5f35750630
Kelvin Pichinini <kelvinpichinini@gmail.com>
2023-03-29 10:04:05 -0300

    Funcao divisao criada

--- MODULO_3_TECNICAS_PROGRAMAÇÃO/Projetos/Calculadora/funcoes.py
+++ MODULO_3_TECNICAS_PROGRAMAÇÃO/Projetos/Calculadora/funcoes.py
@@ -11,3 +11,13 @@ def subtracao(a,b):
     except TypeError:
         print(f"O input 'a' e 'b' devem ser números, recebido {a}: {type(a)}, {b}: {type(b)} ")
         return 'ERRO'
+    
+def divisao(a,b):
+    if b == 0:
+        return 0
+    else:
+        try:
+            return a/b
+        except TypeError:
+            print(f"O input 'a' e 'b' devem ser números, recebido {a}: {type(a)}, {b}: {type(b)} ")
+            return 'ERRO' 
