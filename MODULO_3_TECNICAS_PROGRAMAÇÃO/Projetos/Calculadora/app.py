5884bda7c8cc77533093577261a076c4d1e3d1d4
Kelvin Pichinini <kelvinpichinini@gmail.com>
2023-03-29 10:55:59 -0300

    Funcao calcule impementada

--- MODULO_3_TECNICAS_PROGRAMAÇÃO/Projetos/Calculadora/app.py
+++ MODULO_3_TECNICAS_PROGRAMAÇÃO/Projetos/Calculadora/app.py
@@ -1,1 +1,28 @@
 from funcoes import *
+
+operations = {
+    "soma":soma,
+    "subtracao":subtracao,
+    "multiplicacao":multiplicacao,
+    "divisao":divisao,
+    '+':soma,
+    '-':subtracao,
+    '/':divisao,
+    '*':multiplicacao
+}
+
+def calcule():
+    a = input("Digite o primeiro número: ")
+    operation = input('Qual operação? ')
+    b = input("Digite o segundo numero: ")
+    try:
+        a = float(a)
+        b = float(b)
+    except:
+        print("Números inválidos")
+    try:
+        print(operations[operation](a,b))
+    except:
+        print("Operação inválida")
+
+calcule()
